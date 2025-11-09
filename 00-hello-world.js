4d4675906929b4025948f236fe8196f3a76df2ad
Cecilia <ravacecilia1@gmail.com>
2025-11-09 14:11:01 -0300

    first commit

---
+++ 00-hello-world.js
@@ -0,0 +1,71 @@
+// 00-hello-world.js
+
+// 1. Write a single-line comment
+// Hi! I'm a single line comment
+
+// 2. Write a multi-line comment
+/* Hi! I'm a multiline
+comment */
+
+// 3. Declare variables with values for all primitive data types
+let myString = 'Hello'
+let myNumber = 9
+let myNull = null
+let myUndefined = undefined
+let myBoolean = true
+let mySymbol = Symbol("I'm a symbol")
+let myBigInt = 3456787654345678765434567876543456787654345678765434567876543456787654
+
+/* 4. Log the value of all variables to the console
+console.log(myString)
+console.log(myNumber)
+console.log(myNull)
+console.log(myUndefined)
+console.log(myBoolean)
+console.log(mySymbol)
+console.log(myBigInt)
+*/
+
+/* 5. Log the type of all variables to the console
+console.log(typeof myString)
+console.log(typeof myNumber)
+console.log(typeof myNull)
+console.log(typeof myUndefined)
+console.log(typeof myBoolean)
+console.log(typeof mySymbol)
+console.log(typeof myBigInt)
+*/
+
+// 6. Next, change the variables' values to other values of the same type
+myString = 'Bye'
+myNumber = 2
+myNull = null
+myUndefined = undefined
+myBoolean = false
+mySymbol = Symbol("I'm a happy symbol")
+myBigInt = 1456787654345678765434567876543456787654345678765434567876543456787654
+
+// 7. Next, change the variables' values to values of a different type
+myString = 2
+myNumber = null
+myNull = 'Bye'
+myUndefined = true
+myBoolean = 3
+mySymbol = 4
+myBigInt = 1456
+
+// 8. Declare constants with values for all primitive data types
+const MYSTRING = 'Hello'
+const MYNUMBER = 9
+const MYNULL = null
+const MYUNDEFINED = undefined
+const MYBOOLEAN = true
+const MYSYMBOL = Symbol("I'm a symbol")
+const MYBIGINT = 3456787654345678765434567876543456787654345678765434567876543456787654
+
+// 9. Next, change the values of the constants
+// I can't do that
+
+// 10. Comment out the lines that produce any kind of error when executed
+
+
