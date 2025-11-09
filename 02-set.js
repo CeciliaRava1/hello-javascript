8f3f74267480c458b73506267a798d40199df4f5
Cecilia <ravacecilia1@gmail.com>
2025-11-09 18:40:53 -0300

    feat: Learning about sets

---
+++ 02-set.js
@@ -0,0 +1,11 @@
+// 02-set.js
+
+let mySet = new Set()
+mySet = new Set(['Cat', 'Dog', 'Fox', 'Garfield'])
+console.log(mySet)
+
+// Common methods
+mySet.add('New animal')
+console.log(mySet)
+mySet.delete('Cat')
+console.log(mySet)
