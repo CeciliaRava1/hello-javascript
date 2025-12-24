2d5ac379d4ed632f3224fa6f29a14ba79a67f026
Cecilia <ravacecilia1@gmail.com>
2025-12-24 12:20:25 -0300

    feat: Advance with console methods

---
+++ 10-console-methods.js
@@ -0,0 +1,25 @@
+// 10-console-methods.js
+
+
+// log
+console.log("Hello")
+
+
+// error
+console.error("This is an error message", new Error("Failed conection"))
+
+
+// warn
+console.warn("This is a warning")
+
+
+// info
+console.info("This is an information")
+
+
+// table
+let data = [
+    ["Brais", 37],
+    ["Sarah", 21]
+]
+console.table(data)
